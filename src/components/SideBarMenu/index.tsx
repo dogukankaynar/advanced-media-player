0737e206bd059318e8874b7356ff2c8a94e980d4
dogukankaynar <dogukankaynar98@gmail.com>
2023-06-23 00:53:54 +0300

    change home page responsive css

--- src/components/SideBarMenu/index.tsx
+++ src/components/SideBarMenu/index.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import styles from "./styles.module.css";
 import { useNavigate } from "react-router-dom";
 
-const SideBarMenu:React.FC = () => {
+const SideBarMenu: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <div className={styles.item} onClick={()=>navigate("/")} >
-        <span className={`material-symbols-outlined ${styles.icon}  `}>home</span>
-        Anasayfa
+      <div className={styles.item} onClick={() => navigate("/")}>
+        <span className={`material-symbols-outlined ${styles.icon}  `}>
+          home
+        </span>
+        <p>Anasayfa</p>
       </div>
-      <div className={styles.item} onClick={()=>navigate("/search")}>
+      <span className={`material-symbols-outlined ${styles.menu}`}>collections_bookmark</span>
+      <div className={styles.item} onClick={() => navigate("/search")}>
         <span className={`material-symbols-outlined ${styles.icon}`}>
           search
         </span>
-        Ara
+        <p>Ara</p>
       </div>
     </div>
   );
